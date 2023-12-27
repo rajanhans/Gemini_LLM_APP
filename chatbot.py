39ab63dab8db86112d8e60874dc0e976c16ac941
Rajan Hans <rajhansonline@outlook.com>
2023-12-26 18:44:37 -0800

    updated streamlit calls

--- chatbot.py
+++ chatbot.py
@@ -4,9 +4,11 @@ load_dotenv()
 import streamlit as st
 import os
 import google.generativeai as genai
+api_key =""
 
 with st.sidebar:
-    genai.configure(api_key=st.text_input("Please provide your Gemini Pro API Key"))
+    api_key=st.text_input("Please provide your Gemini Pro API Key")
+    genai.configure(api_key=api_key)
     "[Get a Gemini API key](https://makersuite.google.com/app/apikey)"
    
 
@@ -20,15 +22,22 @@ def get_gemini_respone(question):
     return response
 
 st.title("💬 Rajan's Chatbot-Google Gemini Pro")
+st.divider()
+st.text("!!! Please enter or create a Gemini Pro API key -see sidebar !!!")
+
 
 #Initialize session state for chat history if it doesn't exist
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = [] 
 
-input = st.text_input("Input:", key="input")
-submit=st.button("Ask the question")
+input = st.text_input("Enter your question:", key="input")
+submit=st.button("Ask your question")
+
+
 
 if submit and input:
+    if api_key =="" :
+        st.error("Please enter Gemini Pro Key !")
     response = get_gemini_respone(input)
     ## Add user query and response to session chat history
     st.session_state['chat_history'].append(("You", input))
